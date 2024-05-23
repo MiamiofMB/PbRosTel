c61e4c23a0ee859a848f4b6b7f7d8dfce1402394
Михаил Емельянцев <94082288+MiamiofMB@users.noreply.github.com>
2024-05-23 20:06:13 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,26 @@
 import http.client
-def get_pb():
+def get_pb(id):
    conn = http.client.HTTPSConnection("developer.chargenow.top")
    payload = ''
    headers = {
       'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
       'Authorization': 'Basic aXZhbl9rcmlrdW5vdjpJSzEyMzQ='
    }
-   conn.request("POST", "/cdb-open-api/v1/rent/order/create?deviceId=BJW04969&callbackURL=https://example.com/cdb-open-api/v1/callback", payload, headers)
+   conn.request("POST", f"/cdb-open-api/v1/rent/order/create?deviceId={id}&callbackURL=https://example.com/cdb-open-api/v1/callback", payload, headers)
    res = conn.getresponse()
    data = res.read()
    print(data.decode("utf-8"))
+
+
+def check_pbs(id):
+   #проверить какие айди возвращает и дальше наладить работу с этими айди в функциях ниже.
+   conn = http.client.HTTPSConnection("developer.chargenow.top")
+   payload = ''
+   headers = {
+      'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+      'Authorization': 'Basic aXZhbl9rcmlrdW5vdjpJSzEyMzQ='
+   }
+   conn.request("GET", f"/cdb-open-api/v1/rent/cabinet/query?deviceId={id}", payload, headers)
+   res = conn.getresponse()
+   data = res.read()
+   return data.decode("utf-8")
